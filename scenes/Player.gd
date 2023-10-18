5b8be5bca728119409c244833a74605b5d0fa36b
Jonathan Hecl <jonathanhecl@gmail.com>
2023-10-18 19:08:31 -0300

    Player

---
+++ scenes/Player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+var move_speed: float = 4.0
+var jump_force: float = 8.0
+var gravity: float = 20.0
+
+var facing_angle: float
+
+@onready var model : MeshInstance3D = get_node("CharacterVampire")
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	elif Input.is_action_pressed("jump"):
+		velocity.y = jump_force
+	
+	var input = Input.get_vector("move_left", "move_right", 'move_forward', "move_backward")
+	
+	velocity.x = input.x * move_speed
+	velocity.z = input.y * move_speed
+	
+	move_and_slide()
+	
+	if input.length() > 0:
+		facing_angle = Vector2(input.y, input.x).angle()
+	
+	# model.rotation.y = facing_angle
+	model.rotation.y = lerp_angle(model.rotation.y, facing_angle, 0.5)
+	
+	if global_position.y < -5:
+		game_over()
+
+func game_over():
+	get_tree().reload_current_scene()
